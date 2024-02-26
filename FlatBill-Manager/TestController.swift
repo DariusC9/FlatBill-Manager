807a41bf200df756f8307bbecb88ba5bd0e1765c
Couti Darius <coutidarius@gmail.com>
2024-02-26 15:02:40 +0200

    delete storyboard-main from project

---
+++ FlatBill-Manager/TestController.swift
@@ -0,0 +1,22 @@
+//
+//  TestController.swift
+//  FlatBill-Manager
+//
+//  Created by Darius Couti on 26.02.2024.
+//
+
+import UIKit
+
+class TestController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+    }
+    
+    private func style() {
+        view.backgroundColor = UIColor.systemOrange
+    }
+}
+
