26d752375326afd8b263eafc98b2c489f15da804
Couti Darius <coutidarius@gmail.com>
2024-02-26 14:42:13 +0200

    add Xcode project

---
+++ FlatBill-Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatBill-Manager
+//
+//  Created by Darius Couti on 26.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
